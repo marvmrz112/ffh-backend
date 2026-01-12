c3366985641528edc4066cfacde005c480c46526
Marvin Merz <marvin.merz@feuerwehr-hornau.de>
2026-01-12 17:40:38 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ffh-crew-app',
+  webDir: 'dist'
+};
+
+export default config;
